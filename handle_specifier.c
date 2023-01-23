d45cb980f77c649b9714e98136cc625a4b61bac9
segunlawal <lawalsegun72@gmail.com>
2023-01-23 15:39:29 -0800

    assign function to specifier

--- handle_specifier.c
+++ handle_specifier.c
@@ -1,14 +1,27 @@
+#include "main.h"
 /**
- * handle_specifier - check if character is a valid specifier and assign 
+ * handle_specifier - check if character is a valid specifier and assign
  * function
  * @format: character
  * Return: pointer to function
  */
 
-int (*handle_specifier(char*))(va_list)
+int (*handle_specifier(char *format))(va_list)
 {
-	if (char == '%')
+	int i = 0;
+	func_s handler[3] = {
+		{'c', prt_char},
+		{'s', print_str},
+		{'%', print_cent}
+	};
+
+	for (i = 0; i < 3 ; i++)
 	{
-		//do something
+		if (*format == handler[i].sp)
+		{
+			return (handler[i].f);
+		}
+
 	}
+	return (NULL);
 }
