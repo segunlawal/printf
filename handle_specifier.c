26d388401c7e8f42a76aefa8597f473ffa6c4a04
segunlawal <lawalsegun72@gmail.com>
2023-01-23 15:01:40 -0800

    add specifier handler

---
+++ handle_specifier.c
@@ -0,0 +1,14 @@
+/**
+ * handle_specifier - check if character is a valid specifier and assign 
+ * function
+ * @format: character
+ * Return: pointer to function
+ */
+
+int (*handle_specifier(char*))(va_list)
+{
+	if (char == '%')
+	{
+		//do something
+	}
+}
