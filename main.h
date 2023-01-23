5addd1fa058b77157d7414ccc9049517bbf7c486
segunlawal <lawalsegun72@gmail.com>
2023-01-23 15:13:57 -0800

    update header file

--- main.h
+++ main.h
@@ -5,6 +5,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * struct func - Struct for specifier
+ * @sp: format specifier
+ * @f: the relevant function
+ */
+typedef struct func
+{
+	char *sp;
+	int (*f)(va_list);
+} func_s;
+
 int _printf(const char *format, ...);
+int (*handle_specifier(char*))(va_list);
 
 #endif
